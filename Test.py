da460e72e89d214a759447a2b7887d0f5a98c87f
FinniSpinni <finnjonas.vahle@gmail.com>
2025-07-08 22:51:13 +0200

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,23 @@
+# ordner_logic.py
+
+Zahlen = []
+Woerter = []
+
+def sortiere_text(eingabe):
+    teile = eingabe.strip().split()
+    
+    for teil in teile:
+        if teil.isdigit():
+            Zahlen.append(int(teil))
+        else:
+            Woerter.append(teil)
+    Zahlen.sort()
+
+def get_ergebnisse():
+    return Zahlen, Woerter
+
+def reset():
+    Zahlen.clear()
+    Woerter.clear()
+
+
